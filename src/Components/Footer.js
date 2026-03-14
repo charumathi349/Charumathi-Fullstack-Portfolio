3c4e1f400d5c42b8973c11105ac963a922bdf738
charumathi349 <charumathir44@gmail.com>
2026-03-14 15:45:10 +0530

    updated css layout

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,32 +4,53 @@ import { BsLinkedin } from 'react-icons/bs';
 import '../Components/Footer.css';
 
 const Footer = () => {
-  return (
-    <footer>
-      <a href="#" className="footer_logo">CHARUMATHI</a>
+    return ( <
+        footer >
+        <
+        a href = "#"
+        className = "footer_logo" > CHARUMATHI R < /a>
 
-      <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+        <
+        ul className = "permalinks" >
+        <
+        li > < a href = "#" > Home < /a></li >
+        <
+        li > < a href = "#about" > About < /a></li >
+        <
+        li > < a href = "#experience" > Skills < /a></li >
+        <
+        li > < a href = "#services" > Services < /a></li >
+        <
+        li > < a href = "#contact" > Contact < /a></li >
+        <
+        /ul>
 
-      <div className="footer_socials">
-        <a href="mailto:charumathir44@gmail.com" target="_blank" rel="noreferrer">
-          <MdOutlineEmail />
-        </a>
-        <a href="https://www.linkedin.com/in/charumathi-r-aa10b7298" target="_blank" rel="noreferrer">
-          <BsLinkedin />
-        </a>
-      </div>
+        <
+        div className = "footer_socials" >
+        <
+        a href = "mailto:charumathir44@gmail.com"
+        target = "_blank"
+        rel = "noreferrer" >
+        <
+        MdOutlineEmail / >
+        <
+        /a> <
+        a href = "https://www.linkedin.com/in/charumathi-r-aa10b7298"
+        target = "_blank"
+        rel = "noreferrer" >
+        <
+        BsLinkedin / >
+        <
+        /a> < /
+        div >
 
-      <div className="footer_copyright">
-        <small>&copy; CHARUMATHI. All rights reserved.</small>
-      </div>
-    </footer>
-  );
+        <
+        div className = "footer_copyright" >
+        <
+        small > & copy; CHARUMATHI.All rights reserved. < /small> < /
+        div > <
+        /footer>
+    );
 };
 
 export default Footer;
